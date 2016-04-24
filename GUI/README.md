9b7e78227e1cd2bc107477fbccebde9b43b0aed7
MF57 <p-bochenek@outlook.com>
2016-04-24 02:16:59 +0200

    added  mqtt.js

---
+++ GUI/README.md
@@ -0,0 +1,9 @@
+Project P.I.O.T.W.I.O
+
+bower install
+npm install
+(sudo if failed)
+
+mosca -v --http-port 3000 --http-bundle --http-static ./ | bunyan
+
+(to start test server)
--- GUI/scripts/app.js
+++ GUI/scripts/app.js
@@ -6,4 +6,18 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MainController', ['$scope', function ($scope) {
 
+
+    var client  = mqtt.connect('ws://localhost:3000');
+
+    client.on('connect', function () {
+        client.subscribe('presence');
+        client.publish('presence', 'Hello mqtt');
+    });
+
+    client.on('message', function (topic, message) {
+        // message is Buffer
+        console.log(message.toString());
+        client.end();
+    });
+
 }]);
