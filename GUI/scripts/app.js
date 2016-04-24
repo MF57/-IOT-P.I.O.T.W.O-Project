7fbbf954b33859c58982829f7b880bd5640bdb0f
MF57 <p-bochenek@outlook.com>
2016-04-24 11:40:31 +0200

    board paramethers

--- GUI/scripts/app.js
+++ GUI/scripts/app.js
@@ -5,19 +5,46 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainController', ['$scope', function ($scope) {
+    
+    $scope.width = 2;
+    $scope.height = 3;
+
+    $scope.nodes = [];
 
 
-    var client  = mqtt.connect('ws://localhost:3000');
 
-    client.on('connect', function () {
-        client.subscribe('presence');
-        client.publish('presence', 'Hello mqtt');
-    });
+    $scope.refreshBoard = function() {
+        $scope.nodes = [];
+        for (var i = 0; i < $scope.height; i++) {
+            var row = [];
+            for (var j = 0; j < $scope.width; j++) {
+                var node = {};
+                node.ip = '';
+                node.animation = [];
+                node.row = i;
+                node.column = j;
+                row.push(node);
+            }
+            $scope.nodes.push(row);
+        }
+        console.log($scope.nodes);
+    };
 
-    client.on('message', function (topic, message) {
-        // message is Buffer
-        console.log(message.toString());
-        client.end();
-    });
+
+    // var client  = mqtt.connect('ws://localhost:3000');
+    //
+    // client.on('connect', function () {
+    //     client.subscribe('presence');
+    //     client.publish('presence', 'Hello mqtt');
+    // });
+    //
+    // client.on('message', function (topic, message) {
+    //     // message is Buffer
+    //     console.log(message.toString());
+    //     client.end();
+    // });
+
+
+    $scope.refreshBoard();
 
 }]);
